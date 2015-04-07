dae5f6a2c16bc69b311a252dc082fa5c670844ad
GilesMetcalf <giles@lughnasadh.com>
2015-04-07 09:38:30 +0100

    Create fastregister.php

---
+++ views/fastregister.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* This file is part of the "Approved Users" plugin for Wolf CMS.
+* Licensed under an MIT style license. For full details see license.txt.
+*
+* @author Giles Metcalf <giles@lughnasadh.com>
+* @copyright Giles Metcalf, 2015
+*
+* Original authors:
+*
+* @author Andrew Waters <andrew@band-x.org>
+* @copyright Andrew Waters, 2009
+*
+* @author Martijn van der Kleijn <martijn.niji@gmail.com>
+* @copyright Martijn van der Kleijn, 2009-2013
+*/
+/**
+* This is a complete new view for the Fast Register database load administrative page
+* It allows an administrator to upload a new fast register CSV file and update the database
+* TODO: All of it, really...
+**/
+?>
