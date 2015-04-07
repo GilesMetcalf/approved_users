c9f4975e59119006a1a3382af706c19b1d5821ec
GilesMetcalf <giles@lughnasadh.com>
2015-04-07 10:15:37 +0100

    Update fastregister.php

--- views/fastregister.php
+++ views/fastregister.php
@@ -20,3 +20,18 @@
 * TODO: All of it, really...
 **/
 ?>
+
+<h1><?php echo __('Load FastRegister Database'); ?></h1>
+<p>
+Use this  form to select a CSV file of membership numbers and postcodes to update the Fast Register database.
+</p>
+<p>Note that the CSV file must contain just two columns, the membership number and the postcode. No header row is required.</p>
+
+<form id="fastregisterload" action="<?php echo get_url('plugin/approved_users/fastregisterload/'); ?>" method="post" enctype="multipart/form-data">
+
+    Select fileto upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+
+<input id="commit" name ="uploadfile" class="btn submit" type="submit" accesskey="s" value="Upload FR file" />
+
+</form>
