83529f438de404027a4dad301847806509f8b4e0
GilesMetcalf <giles@lughnasadh.com>
2015-04-08 09:30:09 +0100

    Update fastregister.php

--- views/fastregister.php
+++ views/fastregister.php
@@ -17,21 +17,30 @@
 /**
 * This is a complete new view for the Fast Register database load administrative page
 * It allows an administrator to upload a new fast register CSV file and update the database
-* TODO: All of it, really...
 **/
 ?>
 
 <h1><?php echo __('Load FastRegister Database'); ?></h1>
 <p>
-Use this  form to select a CSV file of membership numbers and postcodes to update the Fast Register database.
+A CSV file of membership numbers and postcodes must be processed to populate the Fast Register database. 
+The CSV file must contain just two columns, the membership number and the postcode. No header row is required.
+</p><p>
+The file can be exported from Excel, generated from another database, or hand-built in a text editor. 
+The completed file must be uploaded to the "public/uploads" file area using Wolf administrative tools.
+</p><p>
+Use this  form to enter the name of the CSV file  to update the Fast Register database. Do not enter the path, as 
+this is generated internally.
 </p>
-<p>Note that the CSV file must contain just two columns, the membership number and the postcode. No header row is required.</p>
+<p>&nbsp; </p>
 
 <form id="fastregisterload" action="<?php echo get_url('plugin/approved_users/fastregisterload/'); ?>" method="post" enctype="multipart/form-data">
 
-    Select fileto upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    Enter filename to process:
+    <input type="text" name="fileToUpload" id="fileToUpload"> 
 
-<input id="commit" name ="uploadfile" class="btn submit" type="submit" accesskey="s" value="Upload FR file" />
+<input id="commit" name ="uploadfile" class="btn submit" type="submit" accesskey="s" value="Process FR file" />
 
 </form>
+
+<p>&nbsp; </p>
+<p><strong>Note:</strong> Very large files of data may take some time to process. Please be patient - you will be notified when it is complete!</p>
